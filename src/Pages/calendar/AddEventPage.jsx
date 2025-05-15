6534fe56bac5295993706070dba9f9cd13a0f677
Jack-18888 <jack.shi.smart@gmail.com>
2025-05-14 20:58:02 -0400

    added ad event functionality

--- src/Pages/calendar/AddEventPage.jsx
+++ src/Pages/calendar/AddEventPage.jsx
@@ -1,6 +1,15 @@
+import EventForm from "../../Components/EventForm";
+import Navbar from "../../Components/Navbar";
+
 function AddEventPage({ addEvent }) {
   return (
-    <div>Add event page</div>
+    <div className="flex h-screen">
+      <Navbar />
+      <div className="flex flex-col w-full items-center gap-5 pt-24">
+        <h1 className="mb-4 text-4xl font-extrabold"> Add Your Event </h1>
+        <EventForm addEvent={addEvent}/>
+      </div>
+    </div>
   );
 }
 
--- src/Pages/calendar/CalendarPage.jsx
+++ src/Pages/calendar/CalendarPage.jsx
@@ -2,16 +2,23 @@ import Navbar from "../../Components/Navbar";
 import Calendar from "../../Components/Calendar"
 import EventList from "../../Components/EventList"
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function CalendarPage({ events }) {
   const [renderedEvents, setRenderedEvents] = useState([]);
+  const navigate = useNavigate();
 
   console.log(events);
   
   return (
     <div className="flex h-screen">
       <Navbar />
-      <Calendar events={events} setRenderedEvents={setRenderedEvents} />
+      <div className="flex flex-col items-center flex-1 gap-12">
+        <Calendar events={events} setRenderedEvents={setRenderedEvents} />
+        <button 
+        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        onClick={() => navigate("/events/add")}>Add New Task</button>
+      </div>
       <EventList events={renderedEvents} />
     </div>
   );
